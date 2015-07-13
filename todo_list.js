63911ccb06696552eb8fa33a101493b8b944edd1
John Hess & Nicholas Toulouse <pair+alookatommorow.nicoto@devbootcamp.com>
2015-07-13 14:58:19 -0700

    first draft of OOJS todoList, now need to create method .add()

--- todo_list.js
+++ todo_list.js
@@ -1,10 +1,33 @@
-var newTodoList = function() {
-	// ???
-};
+// var newTodoList = function() {
+// 	// ???
+// };
 
 
+function Task(item, val) {
+  this.id = val;
+  this.description = item;
+  this.completed = false;
+}
+
+function TodoList(tasks) {
+  if(!tasks) { tasks = []; }
+  this.tasks = tasks;
+  var counter = this.tasks.length;
+}
+
+TodoList.prototype.list = function() {
+  for (var i = 0; i < this.tasks.length; i++) {
+    console.log("Task : " + JSON.stringify(this.tasks[i]) + "\n");
+  }
+}
+
+TodoList.prototype.complete = function() {
+
+}
 
 // Driver code
 
-
-var todoList = newTodoList();
+var cheese = new Task("cheese", 1);
+var bread = new Task("bread", 2);
+var groceryList = new TodoList([cheese, bread]);
+groceryList.list();
