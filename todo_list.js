bf64aee4627fac19668ae1909e948378ef532351
John Hess & Nicholas Toulouse <pair+alookatommorow.nicoto@devbootcamp.com>
2015-07-13 15:51:06 -0700

    OOJS release 2 completion

--- todo_list.js
+++ todo_list.js
@@ -3,6 +3,10 @@
 // };
 
 
+// TodoList.prototype.add - function that takes a val and item as parameters.
+// It creates a new Task object, passing in the parameters
+// It adds the Task to the tasks array
+
 function Task(item, val) {
   this.id = val;
   this.description = item;
@@ -15,14 +19,30 @@ function TodoList(tasks) {
   var counter = this.tasks.length;
 }
 
+TodoList.prototype.add = function (item, val) {
+  this.tasks.push(new Task(item, val));
+}
+
 TodoList.prototype.list = function() {
   for (var i = 0; i < this.tasks.length; i++) {
     console.log("Task : " + JSON.stringify(this.tasks[i]) + "\n");
   }
 }
 
-TodoList.prototype.complete = function() {
+TodoList.prototype.complete = function(taskname) {
+  for (var i = 0; i < this.tasks.length; i++) {
+    if(this.tasks[i].description == taskname){
+      this.tasks[i].completed = true;
+    }
+  }
+}
 
+TodoList.prototype.remove = function(taskObj) {
+  for (var i = 0; i < this.tasks.length; i++) {
+    if(this.tasks[i] === taskObj){
+    this.tasks.splice(i, 1);
+    }
+  }
 }
 
 // Driver code
@@ -30,4 +50,8 @@ TodoList.prototype.complete = function() {
 var cheese = new Task("cheese", 1);
 var bread = new Task("bread", 2);
 var groceryList = new TodoList([cheese, bread]);
+groceryList.add("lettuce", 3);
 groceryList.list();
+groceryList.complete("lettuce");
+bread.completed = false;
+console.log(bread.completed);
